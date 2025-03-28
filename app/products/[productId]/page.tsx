ba000e348fdafc2d1aa8a3db2588958177c744d6
serhii.kotenko <serega.kotenko@gmail.com>
2025-03-28 17:11:09 +0200

    Dynamic Routing

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,12 @@
+type PageType = {
+  params: Promise<{ productId: string }>;
+};
+
+export default async function PDP({ params }: PageType) {
+  const productId = (await params).productId;
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Product Details {productId}
+    </div>
+  );
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function Products() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Products:
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  );
+}
