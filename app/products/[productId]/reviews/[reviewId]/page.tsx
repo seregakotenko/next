6ae6474fefdea8851696578368775cbb1b745b4b
serhii.kotenko <serega.kotenko@gmail.com>
2025-03-28 17:19:01 +0200

    Nested Dynamic Routing

---
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,15 @@
+type PageType = {
+  params: Promise<{
+    productId: string;
+    reviewId: string;
+  }>;
+};
+
+export default async function Review({ params }: PageType) {
+  const { productId, reviewId } = await params;
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Review {reviewId} for product {productId}
+    </div>
+  );
+}
