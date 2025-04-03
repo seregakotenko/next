b8bb793bb65da7cef7c89e1927613b14deeff3b9
serhii.kotenko <serega.kotenko@gmail.com>
2025-04-03 19:17:51 +0300

    Error refresh

--- app/products/[productId]/reviews/[reviewId]/error.tsx
+++ app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,10 +1,22 @@
 'use client';
 
-export default function ErrorBoundary({ error }: { error: Error }) {
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
+
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void }) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
     <div className="">
       ErrorBoundary
       <p>{error.message}</p>
+      <button onClick={reload}>Try again</button>
     </div>
   );
 }
